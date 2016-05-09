c09d18faaad99b6ad59f4b8929f32c371150eb19
DragonMage52 <spencos29@gmail.com>
2016-05-09 19:35:55 -0400

    Add files via upload

---
+++ connect4.js
@@ -0,0 +1,37 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var board = new Array(6);
+var playerOne;
+var playerTwo;
+var turn = 0;
+var end = 0;
+var play = 0;
+
+$(document).ready(function () {
+    
+    for (var i = 0; i < 6; i++) {
+        for (var j = 0; j < 7; j++) {
+            if (!board[i]) {
+                board[i] = [];
+            }
+            board[i][j] = " ";
+        }
+    }
+
+    $("#btnPlay").click(function () {
+        playerOne = document.getElementById("inputPlayerOne").value;
+        playerTwo = document.getElementById("inputPlayerOne").value;
+        $.mobile.changePage("#pagetwo", {transition: "slide"});
+        $("#playerTurn").text(playerOne + "'s Turn");
+        var bw = $("#board").width() *.75;
+        $("table").css({"height" :bw+"px"});
+        play = 1;
+    });
+    
+});
+
+
