2b2e82d5b34a469e4b37a250996149eafffe54b2
DragonMage52 <spencos29@gmail.com>
2025-05-22 17:52:17 -0400

    Add files via upload

--- js/loadimages.js
+++ js/loadimages.js
@@ -1,8 +1,17 @@
-var imageList = [{ name: "APP DSC00514-Edit.jpg", SourceFile: "./500/APP DSC00514-Edit.jpg", ImageWidth: 500, ImageHeight: 500, CreateDate: "2024:07:27 20:44:50", GPSLatitudeRef: "N", GPSLatitude: 41.07233225, GPSLongitudeRef: "W", GPSLongitude: 71.85732525, Keywords: ['Lighthouse', 'Milky Way'], Sublocation: "Montauk Point State Park", ObjectName: "Milky Way over Mountauk Lighthouse"},{ name: "DSC00236.jpg", SourceFile: "./500/DSC00236.jpg", ImageWidth: 500, ImageHeight: 333, CreateDate: "2024:04:23 19:44:54", GPSLatitudeRef: "N", GPSLatitude: 40.6407064833333, GPSLongitudeRef: "W", GPSLongitude: 73.2508687, Keywords: ['Lighthouse', 'Moon'], Sublocation: "Captree State Park", ObjectName: "Pink Moon Rises Behind Fire Island Lighthouse - 1"},{ name: "DSC00245.jpg", SourceFile: "./500/DSC00245.jpg", ImageWidth: 500, ImageHeight: 500, CreateDate: "2024:04:23 19:46:07", GPSLatitudeRef: "N", GPSLatitude: 40.6407024166667, GPSLongitudeRef: "W", GPSLongitude: 73.2508740666667, Keywords: ['Lighthouse', 'Moon'], Sublocation: "Captree State Park", ObjectName: "Pink Moon Rises Behind Fire Island Lighthouse - 2"},{ name: "DSC00319-Edit.jpg", SourceFile: "./500/DSC00319-Edit.jpg", ImageWidth: 500, ImageHeight: 625, CreateDate: "2024:06:28 23:12:13", GPSLatitudeRef: "N", GPSLatitude: 40.5749284166667, GPSLongitudeRef: "W", GPSLongitude: 73.5719109, Keywords: ['Beach', 'Night', 'Stars'], Sublocation: "Jones Beach State Park", ObjectName: "Cloudy Night at the Beach - 1"},{ name: "DSC00344-Edit-Edit copy.jpg", SourceFile: "./500/DSC00344-Edit-Edit copy.jpg", ImageWidth: 500, ImageHeight: 558, CreateDate: "2024:06:28 23:42:29", GPSLatitudeRef: "N", GPSLatitude: 40.5739749666667, GPSLongitudeRef: "W", GPSLongitude: 73.5746896666667, Keywords: ['Beach', 'Night', 'Stars'], Sublocation: "Jones Beach State Park", ObjectName: "Cloudy Night at the Beach - 2"},{ name: "DSC00409-Edit.jpg", SourceFile: "./500/DSC00409-Edit.jpg", ImageWidth: 500, ImageHeight: 750, CreateDate: "2024:07:18 22:41:15", GPSLatitudeRef: "N", GPSLatitude: 40.6214816166667, GPSLongitudeRef: "W", GPSLongitude: 73.2765698333333, Keywords: ['Beach', 'Milky Way', 'Stars'], Sublocation: "Robert Moses State Park", ObjectName: "Summer Milky Way over Beach Tower"},{ name: "DSC00619-Edit.jpg", SourceFile: "./500/DSC00619-Edit.jpg", ImageWidth: 500, ImageHeight: 500, CreateDate: "2024:08:11 20:40:43", GPSLatitudeRef: "N", GPSLatitude: 41.1604865833333, GPSLongitudeRef: "W", GPSLongitude: 72.2332352833333, Keywords: ['Beach', 'Lighthouse', 'Star Trail'], Sublocation: "Orient Point County Park", ObjectName: "Colorful Startrails Above Orient Point Lighthouse"},{ name: "DSC01604-Edit.jpg", SourceFile: "./500/DSC01604-Edit.jpg", ImageWidth: 500, ImageHeight: 333, CreateDate: "2024:10:12 19:25:56", GPSLatitudeRef: "N", GPSLatitude: 40.6212846666667, GPSLongitudeRef: "W", GPSLongitude: 73.2786658666667, Keywords: ['Beach', 'Comet', 'Sunset'], Sublocation: "Robert Moses State Park", ObjectName: "Comet C/2024 S1 At Sunset"},{ name: "DSC01755-Edit-Edit.jpg", SourceFile: "./500/DSC01755-Edit-Edit.jpg", ImageWidth: 500, ImageHeight: 333, CreateDate: "2025:01:05 03:34:27", GPSLatitudeRef: "N", GPSLatitude: 40.9080408333333, GPSLongitudeRef: "W", GPSLongitude: 73.2453648333333, Keywords: ['Night', 'River', 'Stars'], Sublocation: "Sunken Meadow State Park", ObjectName: "Triangle Stars - 1"},{ name: "DSC01805-Edit-Edit.jpg", SourceFile: "./500/DSC01805-Edit-Edit.jpg", ImageWidth: 500, ImageHeight: 400, CreateDate: "2025:01:05 04:19:31", GPSLatitudeRef: "N", GPSLatitude: 40.9085232833333, GPSLongitudeRef: "W", GPSLongitude: 73.2449034833333, Keywords: ['Night', 'Stars'], Sublocation: "Sunken Meadow State Park", ObjectName: "Triangle Stars - 2"},{ name: "DSC01983.jpg", SourceFile: "./500/DSC01983.jpg", ImageWidth: 500, ImageHeight: 214, CreateDate: "2025:04:22 19:40:00", GPSLatitudeRef: "N", GPSLatitude: 40.6422492166667, GPSLongitudeRef: "W", GPSLongitude: 73.2640451166667, Keywords: "Sunset", Sublocation: "Captree State Park", ObjectName: "Sunset at the Beach House"},{ name: "DSC01987.jpg", SourceFile: "./500/DSC01987.jpg", ImageWidth: 500, ImageHeight: 214, CreateDate: "2025:04:22 19:50:38", GPSLatitudeRef: "N", GPSLatitude: 40.6418781666667, GPSLongitudeRef: "W", GPSLongitude: 73.2566559833333, Keywords: ['Dock', 'Sunset'], Sublocation: "Captree State Park", ObjectName: "Sunset at the Harbor"},{ name: "DSC01993.jpg", SourceFile: "./500/DSC01993.jpg", ImageWidth: 500, ImageHeight: 333, CreateDate: "2025:04:22 19:54:08", GPSLatitudeRef: "N", GPSLatitude: 40.6420613333333, GPSLongitudeRef: "W", GPSLongitude: 73.2576323, Keywords: ['Bridge', 'Sunset'], Sublocation: "Captree State Park", ObjectName: "Beached Boat at Sunset - 1"},{ name: "DSC01999.jpg", SourceFile: "./500/DSC01999.jpg", ImageWidth: 500, ImageHeight: 333, CreateDate: "2025:04:22 20:00:27", GPSLatitudeRef: "N", GPSLatitude: 40.6420206333333, GPSLongitudeRef: "W", GPSLongitude: 73.2576752166667, Keywords: ['Bridge', 'Sunset'], Sublocation: "Captree State Park", ObjectName: "Beached Boat at Sunset - 2"},{ name: "DSC02000.jpg", SourceFile: "./500/DSC02000.jpg", ImageWidth: 500, ImageHeight: 333, CreateDate: "2025:04:22 20:01:22", GPSLatitudeRef: "N", GPSLatitude: 40.6420613333333, GPSLongitudeRef: "W", GPSLongitude: 73.2576323, Keywords: ['Bridge', 'Sunset'], Sublocation: "Captree", ObjectName: "Beached Boat at Sunset - 3"},{ name: "DSC02030.jpg", SourceFile: "./500/DSC02030.jpg", ImageWidth: 500, ImageHeight: 214, CreateDate: "2025:04:24 19:49:30", GPSLatitudeRef: "N", GPSLatitude: 40.5998772666667, GPSLongitudeRef: "W", GPSLongitude: 73.5177356, Keywords: ['City', 'Sunset'], Sublocation: "Jones Beach State Park", ObjectName: "Lower Manhatten Skyline at Sunset"},{ name: "DSC02036.jpg", SourceFile: "./500/DSC02036.jpg", ImageWidth: 500, ImageHeight: 281, CreateDate: "2025:04:24 20:04:12", GPSLatitudeRef: "N", GPSLatitude: 40.6021614666667, GPSLongitudeRef: "W", GPSLongitude: 73.5147993, Keywords: ['Dock', 'Sunset'], Sublocation: "Jones Beach State Park", ObjectName: "Sunset Docks"},{ name: "DSC02044.jpg", SourceFile: "./500/DSC02044.jpg", ImageWidth: 500, ImageHeight: 500, CreateDate: "2025:04:27 19:47:26", GPSLatitudeRef: "N", GPSLatitude: 40.7211627, GPSLongitudeRef: "W", GPSLongitude: 73.6422398333333, Keywords: ['Cathedral', 'Moon'], Sublocation: "Cathedral of the Incarnation", ObjectName: "Cathedral of the Moon - 1"},{ name: "DSC02048.jpg", SourceFile: "./500/DSC02048.jpg", ImageWidth: 500, ImageHeight: 333, CreateDate: "2025:04:27 19:48:20", GPSLatitudeRef: "N", GPSLatitude: 40.7211545666667, GPSLongitudeRef: "W", GPSLongitude: 73.6422344666667, Keywords: ['Cathedral', 'Moon'], Sublocation: "Cathedral of the Incarnation", ObjectName: "Cathedral of the Moon - 2"},{ name: "DSC02057.jpg", SourceFile: "./500/DSC02057.jpg", ImageWidth: 500, ImageHeight: 750, CreateDate: "2025:04:27 20:00:17", GPSLatitudeRef: "N", GPSLatitude: 40.7211627, GPSLongitudeRef: "W", GPSLongitude: 73.6422398333333, Keywords: ['Cathedral', 'Moon'], Sublocation: "Cathedral of the Incarnation", ObjectName: "Cathedral of the Moon - 3"}];
+var imageList = [{ name: "APP DSC00514-Edit.jpg", SourceFile: "./500/APP DSC00514-Edit.jpg", ImageWidth: 500, ImageHeight: 500, CreateDate: "2024:07:27 20:44:50", GPSLatitudeRef: "N", GPSLatitude: 41.07233225, GPSLongitudeRef: "W", GPSLongitude: 71.85732525, Keywords: ['Lighthouse', 'Milky Way'], Sublocation: "Montauk Point State Park", ObjectName: "Milky Way over Mountauk Lighthouse"},{ name: "DSC00236.jpg", SourceFile: "./500/DSC00236.jpg", ImageWidth: 500, ImageHeight: 333, CreateDate: "2024:04:23 19:44:54", GPSLatitudeRef: "N", GPSLatitude: 40.6407064833333, GPSLongitudeRef: "W", GPSLongitude: 73.2508687, Keywords: ['Lighthouse', 'Moon'], Sublocation: "Captree State Park", ObjectName: "Pink Moon Rises Behind Fire Island Lighthouse - 1"},{ name: "DSC00245.jpg", SourceFile: "./500/DSC00245.jpg", ImageWidth: 500, ImageHeight: 500, CreateDate: "2024:04:23 19:46:07", GPSLatitudeRef: "N", GPSLatitude: 40.6407024166667, GPSLongitudeRef: "W", GPSLongitude: 73.2508740666667, Keywords: ['Lighthouse', 'Moon'], Sublocation: "Captree State Park", ObjectName: "Pink Moon Rises Behind Fire Island Lighthouse - 2"},{ name: "DSC00319-Edit.jpg", SourceFile: "./500/DSC00319-Edit.jpg", ImageWidth: 500, ImageHeight: 625, CreateDate: "2024:06:28 23:12:13", GPSLatitudeRef: "N", GPSLatitude: 40.5749284166667, GPSLongitudeRef: "W", GPSLongitude: 73.5719109, Keywords: ['Beach', 'Night', 'Stars'], Sublocation: "Jones Beach State Park", ObjectName: "Cloudy Night at the Beach - 1"},{ name: "DSC00344-Edit-Edit copy.jpg", SourceFile: "./500/DSC00344-Edit-Edit copy.jpg", ImageWidth: 500, ImageHeight: 558, CreateDate: "2024:06:28 23:42:29", GPSLatitudeRef: "N", GPSLatitude: 40.5739749666667, GPSLongitudeRef: "W", GPSLongitude: 73.5746896666667, Keywords: ['Beach', 'Night', 'Stars'], Sublocation: "Jones Beach State Park", ObjectName: "Cloudy Night at the Beach - 2"},{ name: "DSC00409-Edit.jpg", SourceFile: "./500/DSC00409-Edit.jpg", ImageWidth: 500, ImageHeight: 750, CreateDate: "2024:07:18 22:41:15", GPSLatitudeRef: "N", GPSLatitude: 40.6214816166667, GPSLongitudeRef: "W", GPSLongitude: 73.2765698333333, Keywords: ['Beach', 'Milky Way', 'Stars'], Sublocation: "Robert Moses State Park", ObjectName: "Summer Milky Way over Beach Tower"},{ name: "DSC00619-Edit.jpg", SourceFile: "./500/DSC00619-Edit.jpg", ImageWidth: 500, ImageHeight: 500, CreateDate: "2024:08:11 20:40:43", GPSLatitudeRef: "N", GPSLatitude: 41.1604865833333, GPSLongitudeRef: "W", GPSLongitude: 72.2332352833333, Keywords: ['Beach', 'Lighthouse', 'Star Trail'], Sublocation: "Orient Point County Park", ObjectName: "Colorful Startrails Above Orient Point Lighthouse"},{ name: "DSC01604-Edit.jpg", SourceFile: "./500/DSC01604-Edit.jpg", ImageWidth: 500, ImageHeight: 333, CreateDate: "2024:10:12 19:25:56", GPSLatitudeRef: "N", GPSLatitude: 40.6212846666667, GPSLongitudeRef: "W", GPSLongitude: 73.2786658666667, Keywords: ['Beach', 'Comet', 'Sunset'], Sublocation: "Robert Moses State Park", ObjectName: "Comet C/2024 S1 At Sunset"},{ name: "DSC01755-Edit-Edit.jpg", SourceFile: "./500/DSC01755-Edit-Edit.jpg", ImageWidth: 500, ImageHeight: 333, CreateDate: "2025:01:05 03:34:27", GPSLatitudeRef: "N", GPSLatitude: 40.9080408333333, GPSLongitudeRef: "W", GPSLongitude: 73.2453648333333, Keywords: ['Night', 'River', 'Stars'], Sublocation: "Sunken Meadow State Park", ObjectName: "Triangle Stars - 1"},{ name: "DSC01805-Edit-Edit.jpg", SourceFile: "./500/DSC01805-Edit-Edit.jpg", ImageWidth: 500, ImageHeight: 400, CreateDate: "2025:01:05 04:19:31", GPSLatitudeRef: "N", GPSLatitude: 40.9085232833333, GPSLongitudeRef: "W", GPSLongitude: 73.2449034833333, Keywords: ['Night', 'Stars'], Sublocation: "Sunken Meadow State Park", ObjectName: "Triangle Stars - 2"},{ name: "DSC01983.jpg", SourceFile: "./500/DSC01983.jpg", ImageWidth: 500, ImageHeight: 214, CreateDate: "2025:04:22 19:40:00", GPSLatitudeRef: "N", GPSLatitude: 40.6422492166667, GPSLongitudeRef: "W", GPSLongitude: 73.2640451166667, Keywords: ["Sunset"], Sublocation: "Captree State Park", ObjectName: "Sunset at the Beach House"},{ name: "DSC01987.jpg", SourceFile: "./500/DSC01987.jpg", ImageWidth: 500, ImageHeight: 214, CreateDate: "2025:04:22 19:50:38", GPSLatitudeRef: "N", GPSLatitude: 40.6418781666667, GPSLongitudeRef: "W", GPSLongitude: 73.2566559833333, Keywords: ['Dock', 'Sunset'], Sublocation: "Captree State Park", ObjectName: "Sunset at the Harbor"},{ name: "DSC01993.jpg", SourceFile: "./500/DSC01993.jpg", ImageWidth: 500, ImageHeight: 333, CreateDate: "2025:04:22 19:54:08", GPSLatitudeRef: "N", GPSLatitude: 40.6420613333333, GPSLongitudeRef: "W", GPSLongitude: 73.2576323, Keywords: ['Bridge', 'Sunset'], Sublocation: "Captree State Park", ObjectName: "Beached Boat at Sunset - 1"},{ name: "DSC01999.jpg", SourceFile: "./500/DSC01999.jpg", ImageWidth: 500, ImageHeight: 333, CreateDate: "2025:04:22 20:00:27", GPSLatitudeRef: "N", GPSLatitude: 40.6420206333333, GPSLongitudeRef: "W", GPSLongitude: 73.2576752166667, Keywords: ['Bridge', 'Sunset'], Sublocation: "Captree State Park", ObjectName: "Beached Boat at Sunset - 2"},{ name: "DSC02000.jpg", SourceFile: "./500/DSC02000.jpg", ImageWidth: 500, ImageHeight: 333, CreateDate: "2025:04:22 20:01:22", GPSLatitudeRef: "N", GPSLatitude: 40.6420613333333, GPSLongitudeRef: "W", GPSLongitude: 73.2576323, Keywords: ['Bridge', 'Sunset'], Sublocation: "Captree", ObjectName: "Beached Boat at Sunset - 3"},{ name: "DSC02030.jpg", SourceFile: "./500/DSC02030.jpg", ImageWidth: 500, ImageHeight: 214, CreateDate: "2025:04:24 19:49:30", GPSLatitudeRef: "N", GPSLatitude: 40.5998772666667, GPSLongitudeRef: "W", GPSLongitude: 73.5177356, Keywords: ['City', 'Sunset'], Sublocation: "Jones Beach State Park", ObjectName: "Lower Manhatten Skyline at Sunset"},{ name: "DSC02036.jpg", SourceFile: "./500/DSC02036.jpg", ImageWidth: 500, ImageHeight: 281, CreateDate: "2025:04:24 20:04:12", GPSLatitudeRef: "N", GPSLatitude: 40.6021614666667, GPSLongitudeRef: "W", GPSLongitude: 73.5147993, Keywords: ['Dock', 'Sunset'], Sublocation: "Jones Beach State Park", ObjectName: "Sunset Docks"},{ name: "DSC02044.jpg", SourceFile: "./500/DSC02044.jpg", ImageWidth: 500, ImageHeight: 500, CreateDate: "2025:04:27 19:47:26", GPSLatitudeRef: "N", GPSLatitude: 40.7211627, GPSLongitudeRef: "W", GPSLongitude: 73.6422398333333, Keywords: ['Cathedral', 'Moon'], Sublocation: "Cathedral of the Incarnation", ObjectName: "Cathedral of the Moon - 1"},{ name: "DSC02048.jpg", SourceFile: "./500/DSC02048.jpg", ImageWidth: 500, ImageHeight: 333, CreateDate: "2025:04:27 19:48:20", GPSLatitudeRef: "N", GPSLatitude: 40.7211545666667, GPSLongitudeRef: "W", GPSLongitude: 73.6422344666667, Keywords: ['Cathedral', 'Moon'], Sublocation: "Cathedral of the Incarnation", ObjectName: "Cathedral of the Moon - 2"},{ name: "DSC02057.jpg", SourceFile: "./500/DSC02057.jpg", ImageWidth: 500, ImageHeight: 750, CreateDate: "2025:04:27 20:00:17", GPSLatitudeRef: "N", GPSLatitude: 40.7211627, GPSLongitudeRef: "W", GPSLongitude: 73.6422398333333, Keywords: ['Cathedral', 'Moon'], Sublocation: "Cathedral of the Incarnation", ObjectName: "Cathedral of the Moon - 3"}];
 
-imageList.forEach(addImages)
+var keywordList = [];
+var locationList = [];
 var grid = document.querySelector('.grid');
 var msnry;
+
+imageList.sort(function(a, b) {
+	if(a.CreateDate < b.CreateDate) {return 1;}
+	if(a.CreateDate > b.CreateDate) {return -1;}
+	return 0;
+});
+
+imageList.forEach(addImages)
 
 imagesLoaded(grid, function() {
 	
@@ -24,8 +33,63 @@ function addImages (item, index, arr) {
 	var dateElement = document.createElement("span");
 	var imageElement = document.createElement("img");
 	
+	var listItemElement;
+	var checkboxElement;
+	var labelElement;
 	
 	gridItemElement.classList.add("grid-item");
+	for (var i = 0; i < imageList[index].Keywords.length; i++) {
+		gridItemElement.classList.add(imageList[index].Keywords[i].replaceAll(" ", ""));
+		if(keywordList.indexOf(imageList[index].Keywords[i]) == -1) {
+			keywordList.push(imageList[index].Keywords[i]);
+			
+			listItemElement = document.createElement("li");
+			
+			checkboxElement = document.createElement("input");
+			checkboxElement.type = "checkbox";
+			checkboxElement.id = imageList[index].Keywords[i].replaceAll(" ", "");
+			checkboxElement.name = imageList[index].Keywords[i].replaceAll(" ", "");
+			checkboxElement.value = imageList[index].Keywords[i].replaceAll(" ", "");
+			checkboxElement.classList.add("filterCheckbox")
+			
+			labelElement = document.createElement("label");
+			labelElement.htmlFor = imageList[index].Keywords[i].replaceAll(" ", "");
+			labelElement.textContent = imageList[index].Keywords[i];
+			
+			
+			listItemElement.appendChild(checkboxElement);
+			listItemElement.appendChild(labelElement);
+			
+			document.getElementById("keywordList").appendChild(listItemElement);
+		}
+	}
+	
+	gridItemElement.classList.add(imageList[index].Sublocation.replaceAll(" ", ""));
+	if(locationList.indexOf(imageList[index].Sublocation) == -1) {
+		locationList.push(imageList[index].Sublocation);
+		
+		listItemElement = document.createElement("li");
+			
+		checkboxElement = document.createElement("input");
+		checkboxElement.type = "checkbox";
+		checkboxElement.id = imageList[index].Sublocation.replaceAll(" ", "");
+		checkboxElement.name = imageList[index].Sublocation.replaceAll(" ", "");
+		checkboxElement.value = imageList[index].Sublocation.replaceAll(" ", "");
+		checkboxElement.classList.add("filterCheckbox")
+			
+		labelElement = document.createElement("label");
+		labelElement.htmlFor = imageList[index].Sublocation.replaceAll(" ", "");
+		labelElement.textContent = imageList[index].Sublocation;
+			
+			
+		listItemElement.appendChild(checkboxElement);
+		listItemElement.appendChild(labelElement);
+			
+		document.getElementById("locationList").appendChild(listItemElement);
+	}
+	
+	gridItemElement.id = index;
+	
 	gridBorderElement.classList.add("grid-item-border");
 	titleElement.classList.add("borderTitle");
 	locationElement.classList.add("borderLocation");
@@ -41,9 +105,54 @@ function addImages (item, index, arr) {
 	
 	gridItemElement.appendChild(gridBorderElement);
 	
-	imageElement.src = "images/500/" + imageList[index].name;
+	imageElement.src = "images/grid/" + imageList[index].name;
 	gridItemElement.appendChild(imageElement);
 	grid.append(gridItemElement);
+	
+	
 }
+
+function submitFilter() {
+	
+	var checkboxList = document.querySelectorAll(".filterCheckbox");
+	var filterList = [];
+	var i,j;
+	var match = false;
+	
+	for(i = 0; i < checkboxList.length; i++) {
+		if(checkboxList[i].checked) {
+			filterList.push(checkboxList[i].id);
+		}
+	}
+	
+	var gridList = document.querySelectorAll(".grid-item");
+	
+	alert(filterList);
+	
+	for(i = 0; i < gridList.length; i++) {
+		for(j = 0; j < filterList.length; j++) {
+			if(gridList[i].classList.contains(filterList[j])) {
+				gridList[i].style.display = "inline-block";
+				match = true;
+				break;
+			}
+			if(!match) {
+				gridList[i].style.display = "none";
+			}
+			match = false;
+		}
+	}
+	
+	msnry.layout();
+	
+	/*if(x.style.display === "none") {
+		x.style.display = "inline-block";
+	}
+	else {
+		x.style.display = "none";
+	}
+	msnry.layout();*/
+}
+	
 
 /*<div class="grid-item"><img src="images/500/DSC02057.jpg"></div>*/
