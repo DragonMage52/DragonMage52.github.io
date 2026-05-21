f5cb79c4f8454b228f888453e88b9ba6f76126f4
DragonMage52 <spencos29@gmail.com>
2026-05-21 17:10:19 -0400

    Update loadimages.js

--- js/loadimages.js
+++ js/loadimages.js
@@ -1,9 +1,8 @@
 var imageList = [{ name: "APP DSC00514-Edit.jpg", SourceFile: "./500/APP DSC00514-Edit.jpg", ImageWidth: 500, ImageHeight: 500, CreateDate: "2024:07:27 20:44:50", GPSLatitudeRef: "N", GPSLatitude: 41.07233225, GPSLongitudeRef: "W", GPSLongitude: 71.85732525, Keywords: ['Lighthouse', 'Milky Way'], Sublocation: "Montauk Point State Park", ObjectName: "Milky Way over Mountauk Lighthouse"},{ name: "DSC00236.jpg", SourceFile: "./500/DSC00236.jpg", ImageWidth: 500, ImageHeight: 333, CreateDate: "2024:04:23 19:44:54", GPSLatitudeRef: "N", GPSLatitude: 40.6407064833333, GPSLongitudeRef: "W", GPSLongitude: 73.2508687, Keywords: ['Lighthouse', 'Moon'], Sublocation: "Captree State Park", ObjectName: "Pink Moon Rises Behind Fire Island Lighthouse - 1"},{ name: "DSC00245.jpg", SourceFile: "./500/DSC00245.jpg", ImageWidth: 500, ImageHeight: 500, CreateDate: "2024:04:23 19:46:07", GPSLatitudeRef: "N", GPSLatitude: 40.6407024166667, GPSLongitudeRef: "W", GPSLongitude: 73.2508740666667, Keywords: ['Lighthouse', 'Moon'], Sublocation: "Captree State Park", ObjectName: "Pink Moon Rises Behind Fire Island Lighthouse - 2"},{ name: "DSC00319-Edit.jpg", SourceFile: "./500/DSC00319-Edit.jpg", ImageWidth: 500, ImageHeight: 625, CreateDate: "2024:06:28 23:12:13", GPSLatitudeRef: "N", GPSLatitude: 40.5749284166667, GPSLongitudeRef: "W", GPSLongitude: 73.5719109, Keywords: ['Beach', 'Night', 'Stars'], Sublocation: "Jones Beach State Park", ObjectName: "Cloudy Night at the Beach - 1"},{ name: "DSC00344-Edit-Edit copy.jpg", SourceFile: "./500/DSC00344-Edit-Edit copy.jpg", ImageWidth: 500, ImageHeight: 558, CreateDate: "2024:06:28 23:42:29", GPSLatitudeRef: "N", GPSLatitude: 40.5739749666667, GPSLongitudeRef: "W", GPSLongitude: 73.5746896666667, Keywords: ['Beach', 'Night', 'Stars'], Sublocation: "Jones Beach State Park", ObjectName: "Cloudy Night at the Beach - 2"},{ name: "DSC00409-Edit.jpg", SourceFile: "./500/DSC00409-Edit.jpg", ImageWidth: 500, ImageHeight: 750, CreateDate: "2024:07:18 22:41:15", GPSLatitudeRef: "N", GPSLatitude: 40.6214816166667, GPSLongitudeRef: "W", GPSLongitude: 73.2765698333333, Keywords: ['Beach', 'Milky Way', 'Stars'], Sublocation: "Robert Moses State Park", ObjectName: "Summer Milky Way over Beach Tower"},{ name: "DSC00619-Edit.jpg", SourceFile: "./500/DSC00619-Edit.jpg", ImageWidth: 500, ImageHeight: 500, CreateDate: "2024:08:11 20:40:43", GPSLatitudeRef: "N", GPSLatitude: 41.1604865833333, GPSLongitudeRef: "W", GPSLongitude: 72.2332352833333, Keywords: ['Beach', 'Lighthouse', 'Star Trail'], Sublocation: "Orient Point County Park", ObjectName: "Colorful Startrails Above Orient Point Lighthouse"},{ name: "DSC01604-Edit.jpg", SourceFile: "./500/DSC01604-Edit.jpg", ImageWidth: 500, ImageHeight: 333, CreateDate: "2024:10:12 19:25:56", GPSLatitudeRef: "N", GPSLatitude: 40.6212846666667, GPSLongitudeRef: "W", GPSLongitude: 73.2786658666667, Keywords: ['Beach', 'Comet', 'Sunset'], Sublocation: "Robert Moses State Park", ObjectName: "Comet C/2024 S1 At Sunset"},{ name: "DSC01755-Edit-Edit.jpg", SourceFile: "./500/DSC01755-Edit-Edit.jpg", ImageWidth: 500, ImageHeight: 333, CreateDate: "2025:01:05 03:34:27", GPSLatitudeRef: "N", GPSLatitude: 40.9080408333333, GPSLongitudeRef: "W", GPSLongitude: 73.2453648333333, Keywords: ['Night', 'River', 'Stars'], Sublocation: "Sunken Meadow State Park", ObjectName: "Triangle Stars - 1"},{ name: "DSC01805-Edit-Edit.jpg", SourceFile: "./500/DSC01805-Edit-Edit.jpg", ImageWidth: 500, ImageHeight: 400, CreateDate: "2025:01:05 04:19:31", GPSLatitudeRef: "N", GPSLatitude: 40.9085232833333, GPSLongitudeRef: "W", GPSLongitude: 73.2449034833333, Keywords: ['Night', 'Stars'], Sublocation: "Sunken Meadow State Park", ObjectName: "Triangle Stars - 2"},{ name: "DSC01983.jpg", SourceFile: "./500/DSC01983.jpg", ImageWidth: 500, ImageHeight: 214, CreateDate: "2025:04:22 19:40:00", GPSLatitudeRef: "N", GPSLatitude: 40.6422492166667, GPSLongitudeRef: "W", GPSLongitude: 73.2640451166667, Keywords: ["Sunset"], Sublocation: "Captree State Park", ObjectName: "Sunset at the Beach House"},{ name: "DSC01987.jpg", SourceFile: "./500/DSC01987.jpg", ImageWidth: 500, ImageHeight: 214, CreateDate: "2025:04:22 19:50:38", GPSLatitudeRef: "N", GPSLatitude: 40.6418781666667, GPSLongitudeRef: "W", GPSLongitude: 73.2566559833333, Keywords: ['Dock', 'Sunset'], Sublocation: "Captree State Park", ObjectName: "Sunset at the Harbor"},{ name: "DSC01993.jpg", SourceFile: "./500/DSC01993.jpg", ImageWidth: 500, ImageHeight: 333, CreateDate: "2025:04:22 19:54:08", GPSLatitudeRef: "N", GPSLatitude: 40.6420613333333, GPSLongitudeRef: "W", GPSLongitude: 73.2576323, Keywords: ['Bridge', 'Sunset'], Sublocation: "Captree State Park", ObjectName: "Beached Boat at Sunset - 1"},{ name: "DSC01999.jpg", SourceFile: "./500/DSC01999.jpg", ImageWidth: 500, ImageHeight: 333, CreateDate: "2025:04:22 20:00:27", GPSLatitudeRef: "N", GPSLatitude: 40.6420206333333, GPSLongitudeRef: "W", GPSLongitude: 73.2576752166667, Keywords: ['Bridge', 'Sunset'], Sublocation: "Captree State Park", ObjectName: "Beached Boat at Sunset - 2"},{ name: "DSC02000.jpg", SourceFile: "./500/DSC02000.jpg", ImageWidth: 500, ImageHeight: 333, CreateDate: "2025:04:22 20:01:22", GPSLatitudeRef: "N", GPSLatitude: 40.6420613333333, GPSLongitudeRef: "W", GPSLongitude: 73.2576323, Keywords: ['Bridge', 'Sunset'], Sublocation: "Captree", ObjectName: "Beached Boat at Sunset - 3"},{ name: "DSC02030.jpg", SourceFile: "./500/DSC02030.jpg", ImageWidth: 500, ImageHeight: 214, CreateDate: "2025:04:24 19:49:30", GPSLatitudeRef: "N", GPSLatitude: 40.5998772666667, GPSLongitudeRef: "W", GPSLongitude: 73.5177356, Keywords: ['City', 'Sunset'], Sublocation: "Jones Beach State Park", ObjectName: "Lower Manhatten Skyline at Sunset"},{ name: "DSC02036.jpg", SourceFile: "./500/DSC02036.jpg", ImageWidth: 500, ImageHeight: 281, CreateDate: "2025:04:24 20:04:12", GPSLatitudeRef: "N", GPSLatitude: 40.6021614666667, GPSLongitudeRef: "W", GPSLongitude: 73.5147993, Keywords: ['Dock', 'Sunset'], Sublocation: "Jones Beach State Park", ObjectName: "Sunset Docks"},{ name: "DSC02044.jpg", SourceFile: "./500/DSC02044.jpg", ImageWidth: 500, ImageHeight: 500, CreateDate: "2025:04:27 19:47:26", GPSLatitudeRef: "N", GPSLatitude: 40.7211627, GPSLongitudeRef: "W", GPSLongitude: 73.6422398333333, Keywords: ['Cathedral', 'Moon'], Sublocation: "Cathedral of the Incarnation", ObjectName: "Cathedral of the Moon - 1"},{ name: "DSC02048.jpg", SourceFile: "./500/DSC02048.jpg", ImageWidth: 500, ImageHeight: 333, CreateDate: "2025:04:27 19:48:20", GPSLatitudeRef: "N", GPSLatitude: 40.7211545666667, GPSLongitudeRef: "W", GPSLongitude: 73.6422344666667, Keywords: ['Cathedral', 'Moon'], Sublocation: "Cathedral of the Incarnation", ObjectName: "Cathedral of the Moon - 2"},{ name: "DSC02057.jpg", SourceFile: "./500/DSC02057.jpg", ImageWidth: 500, ImageHeight: 750, CreateDate: "2025:04:27 20:00:17", GPSLatitudeRef: "N", GPSLatitude: 40.7211627, GPSLongitudeRef: "W", GPSLongitude: 73.6422398333333, Keywords: ['Cathedral', 'Moon'], Sublocation: "Cathedral of the Incarnation", ObjectName: "Cathedral of the Moon - 3"}];
 
-var keywordList = [];
-var locationList = [];
-var grid = document.querySelector('.grid');
-var msnry;
+var allKeywords = new Set();
+var allLocations = new Set();
+var activeFilters = [];
 
 imageList.sort(function(a, b) {
 	if(a.CreateDate < b.CreateDate) {return 1;}
@@ -11,143 +10,193 @@ imageList.sort(function(a, b) {
 	return 0;
 });
 
-imageList.forEach(addImages)
-
-imagesLoaded(grid, function() {
+imageList.forEach(image => {
 	
-	msnry = new Masonry(grid, {
-		itemSelector: ".grid-item",
-		columnWidth: ".grid-sizer",
-		percentPosition: true,
-		transitionDuration: "0.8s"
+	image.Keywords.forEach(keyword => {
+		allKeywords.add(keyword);
 	});
+	
+	allLocations.add(image.Sublocation);
 });
 
-function addImages (item, index, arr) {
-	var grid = document.getElementById("grid");
+[...allKeywords]
+	.sort((a,b) => a.localeCompare(b))
+	.forEach(keyword => {
 	
-	var gridItemElement = document.createElement("div");
-	var gridBorderElement = document.createElement("div");
-	var titleElement = document.createElement("span");
-	var locationElement = document.createElement("span");
-	var dateElement = document.createElement("span");
-	var imageElement = document.createElement("img");
+		addFilterItem(
+			"keywordList",
+			keyword
+		);
+});
+
+[...allLocations]
+	.sort((a,b) => a.localeCompare(b))
+	.forEach(location => {
 	
-	var listItemElement;
-	var checkboxElement;
-	var labelElement;
+		addFilterItem(
+			"locationList",
+			location
+		);
+});
+
+document.querySelectorAll("#mySidenav input")
+	.forEach(input => {
 	
-	gridItemElement.classList.add("grid-item");
-	for (var i = 0; i < imageList[index].Keywords.length; i++) {
-		gridItemElement.classList.add(imageList[index].Keywords[i].replaceAll(" ", ""));
-		if(keywordList.indexOf(imageList[index].Keywords[i]) == -1) {
-			keywordList.push(imageList[index].Keywords[i]);
-			
-			listItemElement = document.createElement("li");
-			
-			checkboxElement = document.createElement("input");
-			checkboxElement.type = "checkbox";
-			checkboxElement.id = imageList[index].Keywords[i].replaceAll(" ", "");
-			checkboxElement.name = imageList[index].Keywords[i].replaceAll(" ", "");
-			checkboxElement.value = imageList[index].Keywords[i].replaceAll(" ", "");
-			checkboxElement.classList.add("filterCheckbox")
-			
-			labelElement = document.createElement("label");
-			labelElement.htmlFor = imageList[index].Keywords[i].replaceAll(" ", "");
-			labelElement.textContent = imageList[index].Keywords[i];
-			
-			
-			listItemElement.appendChild(checkboxElement);
-			listItemElement.appendChild(labelElement);
-			
-			document.getElementById("keywordList").appendChild(listItemElement);
-		}
-	}
+	input.addEventListener("change", renderGallery);
+});
+
+
+renderGallery();
+
+function createGalleryItem(item) {
 	
-	gridItemElement.classList.add(imageList[index].Sublocation.replaceAll(" ", ""));
-	if(locationList.indexOf(imageList[index].Sublocation) == -1) {
-		locationList.push(imageList[index].Sublocation);
+	var galleryItem = document.createElement("a");
+	
+	galleryItem.className = "gallery-item";
+	
+	var ratio = item.ImageWidth / item.ImageHeight;
+	
+	if (ratio < 0.85) {
+      galleryItem.classList.add("portrait");
+    } else if (ratio < 1.15) {
+      galleryItem.classList.add("square");
+    } else if (ratio < 2.2) {
+      galleryItem.classList.add("landscape");
+    } else {
+      galleryItem.classList.add("ultrawide");
+    }
+	
+	galleryItem.innerHTML = `
+		<img 
+			src="images/grid/${item.name}"
+			width="${item.ImageWidth}"
+			height="${item.ImageHeight}"
+
+			loading="lazy"
+			decoding="async"
+		>
 		
-		listItemElement = document.createElement("li");
+		<div class="image-border">
+			<div class="frame-top">
+				<span class="borderTitle">${item.ObjectName}</span>
+			</div>
+
+			<div class="frame-bottom">
+				<span class="borderDate">${formatPhotoDate(item.CreateDate)}</span>
+				<span class="borderLocation">ðŸ“ ${item.Sublocation}</span>
+			</div>
+		</div>
+
+	`;
+	
+	galleryItem.addEventListener("click", () => {
+		openOverlay(item);
+	});
+	
+	return galleryItem;
+}
+
+function renderGallery() {
+	
+	var gallery = document.getElementById("gallery");
+	
+	var selectedKeywords = [
+		
+		...document.querySelectorAll(
+			"#keywordList input:checked"
+		)
+	].map(cb => cb.value);
+	
+	var selectedLocations = [
+		
+		...document.querySelectorAll(
+			"#locationList input:checked"
+		)
+	].map(cb => cb.value);
+	
+	
+	
+	var filteredImages = imageList.filter(image => {
+		
+		var keywordMatch = 
 			
-		checkboxElement = document.createElement("input");
-		checkboxElement.type = "checkbox";
-		checkboxElement.id = imageList[index].Sublocation.replaceAll(" ", "");
-		checkboxElement.name = imageList[index].Sublocation.replaceAll(" ", "");
-		checkboxElement.value = imageList[index].Sublocation.replaceAll(" ", "");
-		checkboxElement.classList.add("filterCheckbox")
+			selectedKeywords.length == 0 ||
 			
-		labelElement = document.createElement("label");
-		labelElement.htmlFor = imageList[index].Sublocation.replaceAll(" ", "");
-		labelElement.textContent = imageList[index].Sublocation;
+			selectedKeywords.every(keyword =>  //AND Logic
+			//selectedKeywords.some(keyword => //OR Logic for Keyword selection
+								   
+				image.Keywords.includes(keyword)
+								   
+			);
+		
+		var locationMatch = 
 			
+			selectedLocations.length == 0 ||
 			
-		listItemElement.appendChild(checkboxElement);
-		listItemElement.appendChild(labelElement);
-			
-		document.getElementById("locationList").appendChild(listItemElement);
-	}
+			selectedLocations.includes(image.Sublocation);
+		
+		return (
+			keywordMatch && locationMatch
+		);
+		
+	});
 	
-	gridItemElement.id = index;
+	gallery.innerHTML = "";
 	
-	gridBorderElement.classList.add("grid-item-border");
-	titleElement.classList.add("borderTitle");
-	locationElement.classList.add("borderLocation");
-	dateElement.classList.add("borderDate");
+	filteredImages.forEach(image => {
+		
+		var item = createGalleryItem(image);
+		
+		gallery.appendChild(item);
+		
+	});
 	
-	titleElement.textContent = imageList[index].ObjectName;
-	locationElement.textContent = "📍" + imageList[index].Sublocation;
-	dateElement.textContent = imageList[index].CreateDate;
-	
-	gridBorderElement.appendChild(titleElement);
-	gridBorderElement.appendChild(locationElement);
-	gridBorderElement.appendChild(dateElement);
-	
-	gridItemElement.appendChild(gridBorderElement);
-	
-	imageElement.src = "images/grid/" + imageList[index].name;
-	gridItemElement.appendChild(imageElement);
-	
-	gridItemElement.onclick = () => openOverlay(gridItemElement);
-	
-	grid.append(gridItemElement);
-	
+	$("#gallery").justifiedGallery({
+		rowHeight: 300,
+		maxRowHeight: 500,
+		margins: 12,
+		border: 0,
+		lastRow: "nojustify",
+		captions: false,
+		waitThumbnailsLoad: true
+	});
 	
 }
 
-function submitFilter() {
+function addFilterItem(listId, value) {
 	
-	var checkboxList = document.querySelectorAll(".filterCheckbox");
-	var filterList = [];
-	var i,j;
+	var li = document.createElement("li");
 	
-	for(i = 0; i < checkboxList.length; i++) {
-		if(checkboxList[i].checked) {
-			filterList.push(checkboxList[i].id);
-		}
-	}
-	
-	var gridList = document.querySelectorAll(".grid-item");
-	
-	outerLoop: for(i = 0; i < gridList.length; i++) {
-		for(j = 0; j < filterList.length; j++) {
-			if(gridList[i].classList.contains(filterList[j])) {
-				gridList[i].style.display = "inline-block";
-				continue outerLoop;
-			}
+	li.innerHTML = `
+		<label class="filterLabel">
 			
-			gridList[i].style.display = "none";
-		}
-	}
+			<input
+				type="checkbox"
+				value="${value}"
+			>
+
+			<span
+				class="filterText"
+				title="${value}"
+			>
+
+			${value}
+
+		</label>
+
+	`;
 	
-	msnry.layout();
+	document.getElementById(listId).appendChild(li)
 }
 
 function openOverlay(selectedImg) {
 	
 	var lightbox = document.getElementById("lightbox");
+	var lightboxCard = document.querySelector(".lightbox-card");
 	var lightboxImg = document.getElementById("lightbox-img");
 	var lightboxText = document.getElementById("lightbox-text");
+	var lightboxTitle = document.getElementById("lightbox-title");
+	var mapFrame = document.getElementById("lightbox-map-frame");
 	
 	lightbox.addEventListener("click", (e) => {
 		if (e.target == lightbox) {
@@ -162,12 +211,62 @@ function openOverlay(selectedImg) {
 	});
 	
 	lightboxImg.src = "";
+	lightboxCard.classList.remove("portrait");
+	mapFrame.src = "";
+	
+	lightboxTitle.textContent = selectedImg.ObjectName;
 	lightboxText.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 	
-	lightboxImg.src = "images/full/" + imageList[selectedImg.id].name;
+	var lat = selectedImg.GPSLatitude * ((selectedImg.GPSLatitudeRef == "N") ? 1 : -1);
+	var lng = selectedImg.GPSLongitude * ((selectedImg.GPSLongitudeRef == "E") ? 1 : -1);
+	
+	mapFrame.src = "https://www.google.com/maps/embed/v1/place?key=AIzaSyA-7lgcuYOldRXBEDS4f_YV7Lyp8Ly-Bxk&&q=" + lat + "," + lng + "&zoom=12";
+	
+	if (selectedImg.ImageHeight > selectedImg.ImageWidth) {
+		lightboxCard.classList.add("portrait");
+	}
+	
+	lightboxImg.src = "images/full/" + selectedImg.name;
+	
 	lightbox.classList.add("active");
 	
 }
-	
 
-/*<div class="grid-item"><img src="images/500/DSC02057.jpg"></div>*/
+function formatPhotoDate(dateString) {
+
+  // Convert EXIF format to ISO-like format
+  const fixed = dateString
+    .replace(/^(\d{4}):(\d{2}):(\d{2})/, "$1-$2-$3")
+    .replace(" ", "T");
+
+  const date = new Date(fixed);
+
+  return date.toLocaleString("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric"
+  });
+}
+
+function toggleSideNav() {
+	var sideNav = document.getElementById("mySidenav");
+	var gallery = document.getElementById("gallery");
+	var filterBtn = document.getElementById("filterBtn");
+	
+	if(sideNav.classList.contains("sideHidden")) {
+		
+		sideNav.classList.remove("sideHidden");
+		sideNav.classList.add("sideVisible");
+		gallery.classList.add("galleryShifted");
+		filterBtn.classList.add("active");
+	}
+	
+	else {
+		
+		sideNav.classList.add("sideHidden");
+		sideNav.classList.remove("sideVisible");
+		gallery.classList.remove("galleryShifted");
+		filterBtn.classList.remove("active");
+	}
+}
+
